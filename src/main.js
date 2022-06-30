350c7a42853391ba2d6cc75de8400dd2659937fd
ponycoding <ponycoding.gmail.com>
2022-06-30 16:04:26 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 const Koa = require("koa");
+
+const { APP_PORT } = require("./config/config.default");
+
 const app = new Koa();
 
 app.use((ctx, next) => {
-  ctx.body = "hello api";
+  ctx.body = "hello api1111";
 });
 
-app.listen(3000, () => {
-  console.log("server is running");
+app.listen(APP_PORT, () => {
+  console.log(`server is running on http://localhost:${APP_PORT}`);
 });
